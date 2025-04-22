fd491ebcf106cd5e283f051a99ef898d60d650f2
Bakyt test <bkuruchbekov312@gmail.com>
2025-04-22 16:32:13 +0600

    pechenka-update

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,19 +1,8 @@
 const cookie = document.getElementById('cookie');
 const counter = document.getElementById('clicker__counter');
-let clickCount = 0;
-let isIncreasing = true;
-const baseSize = 200; // Базовый размер печеньки (из атрибута width)
-const scaleFactor = 0.95; // Коэффициент масштабирования при клике
+let clickCount = 0; // Инициализируем счетчик
 
 cookie.addEventListener('click', () => {
-  clickCount++;
-  counter.textContent = clickCount;
-
-  if (isIncreasing) {
-    cookie.style.width = `${baseSize * scaleFactor}px`;
-  } else {
-    cookie.style.width = `${baseSize}px`;
-  }
-
-  isIncreasing = !isIncreasing; // Инвертируем состояние для следующего клика
+    cookie.style.width = (++clickCount % 2) ? '210px' : '200px'; // Изменяем размер
+    counter.textContent = clickCount; // Обновляем счетчик
 });
